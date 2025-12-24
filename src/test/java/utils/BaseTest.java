b4222728d83cea7ba6f24a540f2576c07766bb24
Vylor <vylor@vylor.ai>
2025-12-24 20:47:36 +0000

    Refactored BaseTest class to externalize Chrome options and base URL, remove commented code, and update lifecycle management.

--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -4,41 +4,35 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
     protected WebDriver driver;
-
-    @BeforeClass
-    public void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--incognito");
-        options.addArguments("--ignore-certificate-errors");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(options);
-        //driver =  new ChromeDriver();
-    }
+    private static final String BASE_URL = System.getProperty("base.url", "https://automationexercise.com/");
+    private static final String CHROME_OPTIONS = System.getProperty(
+        "chrome.options",
+        "--remote-allow-origins=*,--incognito,--ignore-certificate-errors"
+    );
 
     @BeforeMethod
-    public void navigate(){
-        driver.get("https://automationexercise.com/");
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        for (String arg : CHROME_OPTIONS.split(",")) {
+            options.addArguments(arg);
+        }
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+        driver.get(BASE_URL);
     }
 
     @AfterMethod
-    public void removeIntialization(){
-        driver.manage().deleteAllCookies();
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
-
-    @AfterClass
-    public void quitFromBrowser(){
-        driver.quit();
-    }
-
 
 }
