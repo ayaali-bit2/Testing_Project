337d3c4a1ac17a37dd950edbb4ee8ae6e56739f2
Vylor <vylor@vylor.ai>
2026-01-13 14:46:13 +0000

    Enhance ProductWithCart.java by adding assertions, implementing explicit waits, and refactoring method names for improved readability and reliability.

--- src/test/java/Pages/ProductWithCart.java
+++ src/test/java/Pages/ProductWithCart.java
@@ -7,165 +7,128 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
 public class ProductWithCart {
-    WebDriver driver;
+    private WebDriver driver;
+    private WebDriverWait wait;
 
     public ProductWithCart(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 
-    By homeCheck = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[1]/a");
+    private By homeLink = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[1]/a");
+    private By productsButton = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[2]/a");
 
-    By productsButton = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[2]/a");
+    private By firstProductImage = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/img");
+    private By secondProductImage = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[3]/div/div[1]/div[1]/img");
+    private By firstProductHoverArea = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[2]/div/div[1]/div[2]/div/a");
+    private By secondProductHoverArea = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[3]/div/div[1]/div[2]/div/a");
 
-    By selectFirstProduct = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/img");
-    By selectSecondProduct = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[3]/div/div[1]/div[1]/img");
+    private By continueShoppingButton = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[1]/div/div/div[3]/button");
+    private By viewCartLink = By.xpath("(//a[@href=\"/view_cart\"])[2]");
 
-    By hoverOnFirstProduct = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[2]/div/div[1]/div[2]/div");
-    By hoverOnSecondProduct = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[3]/div/div[1]/div[2]/div");
+    private By firstProductNameInCart = By.cssSelector("#product-1 > td.cart_description > p");
+    private By secondProductNameInCart = By.cssSelector("#product-2 > td.cart_description > h4 > a");
 
-    By continoueShopping = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[1]/div/div/div[3]/button");
+    private By firstProductPriceInCart = By.cssSelector("#product-1 > td.cart_price > p");
+    private By firstProductQuantityInCart = By.cssSelector("#product-1 > td.cart_quantity > button");
+    private By firstProductTotalPriceInCart = By.cssSelector("#product-1 > td.cart_total > p");
 
-    By viewCartLink = By.xpath("(//a[@href=\"/view_cart\"])[2]");
+    private By secondProductPriceInCart = By.cssSelector("#product-2 > td.cart_price > p");
+    private By secondProductQuantityInCart = By.cssSelector("#product-2 > td.cart_quantity > button");
+    private By secondProductTotalPriceInCart = By.cssSelector("#product-2 > td.cart_total > p");
 
-    By firstProductAddedToCart = By.cssSelector("#product-1 > td.cart_description > p");
-    By secondProductAddedToCart = By.cssSelector("#product-2 > td.cart_description > h4 > a");
+    private By homeProductImage = By.xpath("//img[@src=\"/get_product_picture/2\"]");
+    private By homeAddToCartButton = By.xpath("(//a[@data-product-id=\"2\"])[2]");
 
-    By firstProductPriceInCartPage = By.cssSelector("#product-1 > td.cart_price > p");
-    By firstProductQuantityInCartPage = By.cssSelector("#product-1 > td.cart_quantity > button");
-    By firstProductTotalPriceInCartPage = By.cssSelector("#product-1 > td.cart_total > p");
+    private By cartTable = By.xpath("//td[@class=\"image\"]");
+    private By deleteProductButton = By.xpath("//a[@class=\"cart_quantity_delete\"]");
+    private By emptyCartMessage = By.xpath("//span[@id=\"empty_cart\"]");
 
-    By seconedProductPriceInCartPage = By.cssSelector("#product-2 > td.cart_price > p");
-    By seconedProductQuantityInCartPage = By.cssSelector("#product-2 > td.cart_quantity > button");
-    By seconedProductTotalPriceInCartPage = By.cssSelector("#product-2 > td.cart_total > p");
-
-    By hoverProductFromHome = By.xpath("//img[@src=\"/get_product_picture/2\"]");
-    By clickAddToCartHome = By.xpath("(//a[@data-product-id=\"2\"])[2]");
-
-    By verifyCartPage = By.xpath("//td[@class=\"image\"]");
-    By deleteProductFromCart = By.xpath("//a[@class=\"cart_quantity_delete\"]");
-
-    By verifyCartEmpty = By.xpath("//span[@id=\"empty_cart\"]");
-
-    public void HomeCheck() {
-        System.out.println(driver.findElement(homeCheck).isDisplayed());
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0, 500);");
-
+    public void verifyHomeLinkIsDisplayed() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(homeLink));
+        Assert.assertTrue(driver.findElement(homeLink).isDisplayed(), "Home link is not displayed");
     }
 
-    public void goToProductsPage() {
-        WebElement textField = driver.findElement(productsButton);
-        String value2 = textField.getAttribute("href");
-        driver.navigate().to(value2);
+    public void navigateToProductsPage() {
+        wait.until(ExpectedConditions.elementToBeClickable(productsButton));
+        driver.findElement(productsButton).click();
     }
 
-    public void clickOnAddToCartFirstProduct(){
-        WebElement elementToHover = driver.findElement(selectFirstProduct);
-
-        // Find the element to click (can be the same or revealed after hover)
-        WebElement elementToClick = driver.findElement(By.xpath("/html/body/section[2]/div/div/div[2]/div/div[2]/div/div[1]/div[2]/div/a"));
-
-        // Create Actions instance
+    public void addFirstProductToCart() {
         Actions actions = new Actions(driver);
-
-        // Perform hover and click
-        actions.moveToElement(elementToHover).perform();
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
-        WebElement element = wait.until(
-                ExpectedConditions.elementToBeClickable(hoverOnFirstProduct));
-        elementToClick.click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement product = wait.until(ExpectedConditions.visibilityOfElementLocated(firstProductImage));
+        actions.moveToElement(product).perform();
+        WebElement addButton = wait.until(ExpectedConditions.elementToBeClickable(firstProductHoverArea));
+        addButton.click();
     }
 
-    public void clickOnAddToCartSeconedProduct(){
-        WebElement elementToHover = driver.findElement(selectSecondProduct);
-
-        // Find the element to click (can be the same or revealed after hover)
-        WebElement elementToClick = driver.findElement(hoverOnSecondProduct);
-
-        // Create Actions instance
+    public void addSecondProductToCart() {
         Actions actions = new Actions(driver);
-
-        // Perform hover and click
-        actions.moveToElement(elementToHover).perform();
-        elementToClick.click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+        WebElement product = wait.until(ExpectedConditions.visibilityOfElementLocated(secondProductImage));
+        actions.moveToElement(product).perform();
+        WebElement addButton = wait.until(ExpectedConditions.elementToBeClickable(secondProductHoverArea));
+        addButton.click();
     }
 
-    public void goTocontinoueShopping(){
-//        WebElement textField = driver.findElement(continoueShopping);
-//        textField.click();
-        //driver.switchTo().alert().accept();
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        // Wait until the modal is visible and the button is clickable
-        WebElement continueButton = wait.until(ExpectedConditions.elementToBeClickable(continoueShopping));
-        continueButton.click();
+    public void continueShopping() {
+        WebElement continueBtn = wait.until(ExpectedConditions.elementToBeClickable(continueShoppingButton));
+        continueBtn.click();
     }
 
-    public void goToCart(){
-        WebElement textField = driver.findElement(viewCartLink);
-        String value2 = textField.getAttribute("href");
-        driver.navigate().to(value2);
+    public void navigateToCart() {
+        WebElement cartLink = wait.until(ExpectedConditions.elementToBeClickable(viewCartLink));
+        cartLink.click();
     }
 
-    public void verifyFirstProductAddedToCart(){
-        System.out.println(driver.findElement(firstProductAddedToCart).isDisplayed());
+    public void verifyFirstProductIsAddedToCart() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(firstProductNameInCart));
+        Assert.assertTrue(driver.findElement(firstProductNameInCart).isDisplayed(), "First product not added to cart");
     }
 
-    public void verifySeconedProductAddedToCart(){
-        System.out.println(driver.findElement(secondProductAddedToCart).isDisplayed());
+    public void verifySecondProductIsAddedToCart() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(secondProductNameInCart));
+        Assert.assertTrue(driver.findElement(secondProductNameInCart).isDisplayed(), "Second product not added to cart");
     }
 
-    public void verifyDetailsOfFirstProduct(){
-        System.out.println(driver.findElement(firstProductPriceInCartPage).isDisplayed());
-        System.out.println(driver.findElement(firstProductQuantityInCartPage).isDisplayed());
-        System.out.println(driver.findElement(firstProductTotalPriceInCartPage).isDisplayed());
-
+    public void verifyFirstProductDetails() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(firstProductPriceInCart));
+        Assert.assertTrue(driver.findElement(firstProductPriceInCart).isDisplayed(), "First product price not displayed");
+        Assert.assertTrue(driver.findElement(firstProductQuantityInCart).isDisplayed(), "First product quantity not displayed");
+        Assert.assertTrue(driver.findElement(firstProductTotalPriceInCart).isDisplayed(), "First product total price not displayed");
     }
 
-    public void verifyDetailsOfSecondProduct(){
-        System.out.println(driver.findElement(seconedProductPriceInCartPage).isDisplayed());
-        System.out.println(driver.findElement(seconedProductQuantityInCartPage).isDisplayed());
-        System.out.println(driver.findElement(seconedProductTotalPriceInCartPage).isDisplayed());
-
+    public void verifySecondProductDetails() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(secondProductPriceInCart));
+        Assert.assertTrue(driver.findElement(secondProductPriceInCart).isDisplayed(), "Second product price not displayed");
+        Assert.assertTrue(driver.findElement(secondProductQuantityInCart).isDisplayed(), "Second product quantity not displayed");
+        Assert.assertTrue(driver.findElement(secondProductTotalPriceInCart).isDisplayed(), "Second product total price not displayed");
     }
 
-    public void chooseProductFromHome(){
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
-
-        WebElement elementToHover = driver.findElement(hoverProductFromHome);
-
-        // Find the element to click (can be the same or revealed after hover)
-        WebElement elementToClick = driver.findElement(clickAddToCartHome);
-
-        // Create Actions instance
+    public void addProductFromHome() {
         Actions actions = new Actions(driver);
-
-        // Perform hover and click
-        actions.moveToElement(elementToHover).perform();
-
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(300));
-        WebElement element = wait.until(
-                ExpectedConditions.elementToBeClickable(clickAddToCartHome));
-        elementToClick.click();
+        WebElement product = wait.until(ExpectedConditions.visibilityOfElementLocated(homeProductImage));
+        actions.moveToElement(product).perform();
+        WebElement addButton = wait.until(ExpectedConditions.elementToBeClickable(homeAddToCartButton));
+        addButton.click();
     }
 
-    public void checkVerifyCartPage(){
-        System.out.println(driver.findElement(verifyCartPage).isDisplayed());
+    public void verifyCartPageIsDisplayed() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(cartTable));
+        Assert.assertTrue(driver.findElement(cartTable).isDisplayed(), "Cart page is not displayed");
     }
 
-    public void checkDeleteProductFromCart(){
-        driver.findElement(deleteProductFromCart).click();
+    public void removeProductFromCart() {
+        WebElement deleteBtn = wait.until(ExpectedConditions.elementToBeClickable(deleteProductButton));
+        deleteBtn.click();
     }
 
-    public void checkVerifyCartEmpty(){
-        System.out.println(driver.findElement(verifyCartEmpty).isDisplayed());
+    public void verifyCartIsEmpty() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(emptyCartMessage));
+        Assert.assertTrue(driver.findElement(emptyCartMessage).isDisplayed(), "Cart is not empty");
     }
 }
