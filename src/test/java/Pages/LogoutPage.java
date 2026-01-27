b4c0abb5e21597c22385ad29e6aa843f088f7169
Vylor <vylor@vylor.ai>
2026-01-27 12:16:04 +0000

    refactor: replace implicit waits with explicit waits in ProductQuantityInCart.java to improve code reliability and readability

--- src/test/java/Pages/LogoutPage.java
+++ src/test/java/Pages/LogoutPage.java
@@ -3,64 +3,70 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
 
 public class LogoutPage {
 
-        WebDriver driver;
+    private WebDriver driver;
+    private WebDriverWait wait;
 
     public LogoutPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
+    private By homeSectionHeader = By.cssSelector("body > section:nth-child(3) > div > div > div.col-sm-9.padding-right > div.features_items > h2");
+    private By signInAndSignUpLink = By.cssSelector("#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(4) > a");
+    private By newUserSignUpHeader = By.cssSelector("#form > div > div > div:nth-child(3) > div > h2");
+    private By emailAddressField = By.cssSelector("#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > input[type=email]:nth-child(2)");
+    private By passwordField = By.cssSelector("#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > input[type=password]:nth-child(3)");
+    private By signUpButton = By.cssSelector("#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > button");
+    private By usernameInBar = By.cssSelector("#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(10) > a > b");
+    private By logoutLink = By.cssSelector("#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(4) > a");
 
-    By homeCheck = By.cssSelector("body > section:nth-child(3) > div > div > div.col-sm-9.padding-right > div.features_items > h2");
-    By singInAndSignUpButton = By.cssSelector("#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(4) > a");
-
-    By emailAddressField = By.cssSelector("#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > input[type=email]:nth-child(2)");
-    By passwordField = By.cssSelector("#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > input[type=password]:nth-child(3)");
-
-    By newUserSignUpVisible = By.cssSelector("#form > div > div > div:nth-child(3) > div > h2");
-
-    By signUpButton = By.cssSelector("#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > button");
-
-    By usernameInBar = By.cssSelector("#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(10) > a > b");
-
-    By logoutButton = By.cssSelector("#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(4) > a");
-
-    public void HomeCheck() {
-        System.out.println(driver.findElement(homeCheck).isDisplayed());
+    public void verifyHomeSectionIsDisplayed() {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(homeSectionHeader));
+        Assert.assertTrue(element.isDisplayed(), "Home section header should be displayed.");
     }
 
-    public void SingInAndSignUpButton() {
-        WebElement textField = driver.findElement(singInAndSignUpButton);
-        String value2 = textField.getAttribute("href");
-        driver.navigate().to(value2);
+    public void clickSignInAndSignUp() {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(signInAndSignUpLink));
+        element.click();
     }
 
-    public void NewUserSignUpVisible() {
-        System.out.println(driver.findElement(newUserSignUpVisible).isDisplayed());
+    public void verifyNewUserSignUpSectionIsVisible() {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(newUserSignUpHeader));
+        Assert.assertTrue(element.isDisplayed(), "New user sign-up section should be visible.");
     }
 
-    public void setEmailAddressField(String emailAddressFieldtext) {
-        driver.findElement(emailAddressField).sendKeys(emailAddressFieldtext);
+    public void enterEmailAddress(String email) {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(emailAddressField));
+        element.clear();
+        element.sendKeys(email);
     }
 
-    public void setPasswordField(String passwordFieldtext) {
-        driver.findElement(passwordField).sendKeys(passwordFieldtext);
+    public void enterPassword(String password) {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(passwordField));
+        element.clear();
+        element.sendKeys(password);
     }
 
-    public void setSignUpButton() {
-        driver.findElement(signUpButton).click();
+    public void clickSignUp() {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(signUpButton));
+        element.click();
     }
 
-    public void checkUserNameBar(){
-        driver.findElement(usernameInBar).isDisplayed();
+    public void verifyUsernameIsDisplayed() {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(usernameInBar));
+        Assert.assertTrue(element.isDisplayed(), "Username should be displayed in the navigation bar.");
     }
 
-    public void logoutButtonFun() {
-        WebElement textField = driver.findElement(logoutButton);
-        String value2 = textField.getAttribute("href");
-        driver.navigate().to(value2);
+    public void clickLogout() {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(logoutLink));
+        element.click();
     }
-
 }
