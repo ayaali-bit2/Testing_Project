68fff6e875fe206e0983185ee635e6ea5a5b61b3
Vylor <vylor@vylor.ai>
2026-07-20 15:02:07 +0000

    PR-54: Parameterize ProductQuantityInCart quantity and restore cart verification in Testing_Project

--- src/test/java/Pages/ProductQuantityInCart.java
+++ src/test/java/Pages/ProductQuantityInCart.java
@@ -3,88 +3,65 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
 public class ProductQuantityInCart {
 
-    WebDriver driver;
+    private final WebDriver driver;
 
     public ProductQuantityInCart(WebDriver driver) {
         this.driver = driver;
     }
 
-    String quantity;
-
-    By homeCheck = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[1]/a");
-    By viewProduct = By.cssSelector("body > section:nth-child(3) > div > div > div.col-sm-9.padding-right > div.features_items > div:nth-child(3) > div > div.choose > ul > li > a");
-
-    By productDetailIsOpened = By.cssSelector("body > section > div > div > div.col-sm-9.padding-right > div.product-details > div.col-sm-7 > div > img.newarrival");
-
-    By firstProductQuantity = By.cssSelector("#quantity");
-
-    By addToCartButton = By.cssSelector("body > section > div > div > div.col-sm-9.padding-right > div.product-details > div.col-sm-7 > div > span > button");
-    By viewCartLink = By.cssSelector("#cartModal > div > div > div.modal-body > p:nth-child(2) > a");
-
-    By quantityOfProductInCart = By.cssSelector("#product-1 > td.cart_quantity > button");
+    private final By homeCheck = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[1]/a");
+    private final By viewProduct = By.cssSelector("body > section:nth-child(3) > div > div > div.col-sm-9.padding-right > div.features_items > div:nth-child(3) > div > div.choose > ul > li > a");
+    private final By productDetailIsOpened = By.cssSelector("body > section > div > div > div.col-sm-9.padding-right > div.product-details > div.col-sm-7 > div > img.newarrival");
+    private final By firstProductQuantity = By.cssSelector("#quantity");
+    private final By addToCartButton = By.cssSelector("body > section > div > div > div.col-sm-9.padding-right > div.product-details > div.col-sm-7 > div > span > button");
+    private final By viewCartLink = By.cssSelector("#cartModal > div > div > div.modal-body > p:nth-child(2) > a");
+    private final By quantityOfProductInCart = By.cssSelector("#product-1 > td.cart_quantity > button");
 
     public void HomeCheck() {
-        System.out.println(driver.findElement(homeCheck).isDisplayed());
+        Assert.assertTrue(driver.findElement(homeCheck).isDisplayed(), "Home link should be visible");
     }
 
-    public void clickOnViewProduct(){
+    public void clickOnViewProduct() {
         WebElement textField = driver.findElement(viewProduct);
         String value2 = textField.getAttribute("href");
         driver.navigate().to(value2);
     }
 
-    public void verifyProductDetailIsOpened(){
-        System.out.println(driver.findElement(productDetailIsOpened).isDisplayed());
+    public void verifyProductDetailIsOpened() {
+        Assert.assertTrue(driver.findElement(productDetailIsOpened).isDisplayed(), "Product detail page should be displayed");
     }
 
-    public void clickOnAddToCartFirstProduct(){
-        WebElement elementToHover = driver.findElement(firstProductQuantity);
-
-        // Find the element to click (can be the same or revealed after hover)
-        WebElement elementToClick = driver.findElement(By.cssSelector("#product_id"));
-
-        // Create Actions instance
-        Actions actions = new Actions(driver);
-
-        quantity = "4";
-        // Perform hover and click
-        actions.moveToElement(elementToHover).perform();
-        elementToHover.clear();
-        elementToHover.sendKeys(quantity);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    public void clickOnAddToCartFirstProduct(String quantity) {
+        WebElement quantityInput = new WebDriverWait(driver, Duration.ofSeconds(15))
+                .until(ExpectedConditions.elementToBeClickable(firstProductQuantity));
+        quantityInput.clear();
+        quantityInput.sendKeys(quantity);
     }
 
-    public void clickOnAddToCart(){
-        driver.findElement(addToCartButton).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    public void clickOnAddToCart() {
+        WebElement element = new WebDriverWait(driver, Duration.ofSeconds(20))
+                .until(ExpectedConditions.elementToBeClickable(addToCartButton));
+        element.click();
     }
 
-    public void clickOnViewCart(){
-        WebElement textField = driver.findElement(viewCartLink);
-        String value2 = textField.getAttribute("href");
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
-        WebElement element = wait.until(
-                ExpectedConditions.visibilityOf(textField));
-        driver.navigate().to(value2);
-
-
+    public void clickOnViewCart() {
+        WebElement element = new WebDriverWait(driver, Duration.ofSeconds(20))
+                .until(ExpectedConditions.elementToBeClickable(viewCartLink));
+        element.click();
     }
 
-    public void verifyDetailsOfFirstProduct(){
-        WebElement element = driver.findElement(quantityOfProductInCart);
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
-        WebElement element2 = wait.until(
-                ExpectedConditions.visibilityOf(element));
-        String value = element.getText();
-        System.out.println(value == quantity);
-
+    public void verifyDetailsOfFirstProduct(String expectedQuantity) {
+        WebElement element = new WebDriverWait(driver, Duration.ofSeconds(20))
+                .until(ExpectedConditions.visibilityOfElementLocated(quantityOfProductInCart));
+        String actualQuantity = element.getText().trim();
+        Assert.assertEquals(actualQuantity, expectedQuantity, "Cart quantity should match the requested value");
     }
 }
